c24aa20897da46ae8e7df077394e85bc78c5822f
Ginny Yadav <ginny@tidepool.org>
2020-12-22 10:26:17 -0500

    flat list component

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,16 +2,18 @@ import React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
 const ComponentsScreen = () => {
+    const yourName = 'Ginny';
     return (
         <View>
-            <Text style={styles.textStyle}>Hi Ginger</Text>
+            <Text style={styles.textStyle}>Getting started with react native!</Text>
+            <Text style={{fontSize: 20}}>My name is {yourName}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create ({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
     }
 });
 
