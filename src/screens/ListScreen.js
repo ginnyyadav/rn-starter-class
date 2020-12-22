c6ac930d275b8ef381c3ad5f77d59fd1405709a4
Ginny Yadav <ginny@tidepool.org>
2020-12-22 11:19:50 -0500

    navigation added

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,26 +3,30 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1'}, 
-        {name: 'Friend #2'},
-        {name: 'Friend #3'},
-        {name: 'Friend #4'},
-        {name: 'Friend #5'},
-        {name: 'Friend #6'},
-        {name: 'Friend #7'},
-        {name: 'Friend #8'},
-        {name: 'Friend #9'},
+        {name: 'Friend #1', age: '20'}, 
+        {name: 'Friend #2', age: '45'},
+        {name: 'Friend #3', age: '32'},
+        {name: 'Friend #4', age: '27'},
+        {name: 'Friend #5', age: '53'},
+        {name: 'Friend #6', age: '30'},
+        {name: 'Friend #7', age: '15'},
+        {name: 'Friend #8', age: '62'},
+        {name: 'Friend #9', age: '35'},
     ]
     return (
         <FlatList
             keyExtractor={(friend) => friend.name} 
             data={friends} 
             renderItem={({item}) => {
-                return <Text>{item.name}</Text>
+                return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
             }}
         />)
 };
 
-const styles = StyleSheet.create ({});
+const styles = StyleSheet.create ({
+    textStyle: {
+        marginVertical: 10
+    }
+});
 
 export default ListScreen;
